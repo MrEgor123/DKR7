a076380d9bf8381bed8631d7901e44752c1f503b
MrEgor123 <144677952+MrEgor123@users.noreply.github.com>
2024-02-20 12:33:50 +0300

    Add files via upload

---
+++ DKR_7.pas
@@ -0,0 +1,59 @@
+﻿program DKR_7;
+uses GraphABC, System;
+
+var
+  scale: Real = 1.0; // параметризация
+  depth: Integer = 6;
+  offsetX: Real = 0.0;
+  offsetY: Real = 0.0;
+
+procedure Redraw; // для перерисовки окна
+begin
+  ClearWindow;
+  Draw(Round(100*scale+offsetX), Round(400*scale+offsetY), Round(700*scale+offsetX), Round(400*scale+offsetY), depth);
+end;
+
+procedure Button(Key: Integer); // нажатие кнопок
+begin
+  case Key of
+    VK_Down: begin // вниз
+      offsetY := offsetY - 30;
+      Redraw;
+    end;
+    VK_Up: begin // вверх
+      offsetY := offsetY + 30;
+      Redraw;
+    end;
+    VK_Right: begin // вправо
+      offsetX := offsetX - 30;
+      Redraw;
+    end;
+    VK_Left: begin // влево
+      offsetX := offsetX + 30;
+      Redraw;
+    end;
+    VK_O: begin // увеличение размера
+      scale := scale * 1.5;
+      Redraw;
+    end;
+    VK_P: begin // уменьшение размера
+      scale := scale / 1.5;
+      Redraw;
+    end;
+    VK_W: begin // увеличение глубины
+      depth := depth + 1;
+      Redraw;
+    end;
+    VK_S: if depth > 0 then begin // уменьшение глубины
+      depth := depth - 1;
+      Redraw;
+    end;
+  end;
+end;
+
+begin
+  SetWindowSize(600, 600);
+  SetWindowCaption('Кривая Хартера-Хейтуэя');
+  OnKeyDown := Button;
+  Redraw;
+end.
---
+++ System.pas
@@ -0,0 +1,27 @@
+﻿unit System;
+
+interface
+
+uses GraphABC;
+
+procedure Draw(x1, y1, x2, y2, depth: integer);
+
+implementation
+
+procedure Draw(x1, y1, x2, y2, depth: integer); // для рисования
+begin
+  if depth = 0 then // выделение базы рекурсии
+    Line(x1, y1, x2, y2)
+  else
+  begin
+    // Находим координаты середины отрезка
+    var midX := (x1 + x2) div 2 + (y2 - y1) div 2;
+    var midY := (y1 + y2) div 2 - (x2 - x1) div 2;
+    
+    // Декомпозиция
+    Draw(x1, y1, midX, midY, depth - 1); // от (x1, y1) до середины (midX, midY),
+    Draw(x2, y2, midX, midY, depth - 1); // от (x2, y2) до середины (midX, midY)
+  end;
+end;
+
+end.
